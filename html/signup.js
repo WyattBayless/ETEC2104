39f8e361abb09fc02ae7962a3f1a9be708da6baf
WyattBayless <165172223+WyattBayless@users.noreply.github.com>
2025-02-28 23:08:57 -0500

    dom assignment completed

--- html/signup.js
+++ html/signup.js
@@ -9,28 +9,39 @@ function clicked(){
     let pwordValue = pwordInput.value;
     let dobInput = document.getElementById("dob");
     let dobValue = dobInput.value;
-    checkEmail(emailValue);
-    checkName(nameValue);
-    checkAge(dobValue);
+    if(checkEmail(emailValue)===0 && checkName(nameValue)===0 && checkAge(dobValue)===0) {
+        let valid = document.createElement("div");
+        valid.classList.add("welcome");
+        document.body.appendChild(valid);
+        valid.appendChild( document.createTextNode( "Welcome!"));
+    }
 }
 
 function checkEmail(email){
     let words = email.trim().split("@");
     if (words.length === 2 && words[0].length > 0 && words[1].length > 0){
-        return;
+        return 0;
     }
     else {
-        alert("Must input a proper email address")
+        let invalid = document.createElement("div");
+        invalid.classList.add("alert");
+        document.body.appendChild(invalid);
+        invalid.appendChild( document.createTextNode( "Please enter a valid email"));
+        return 1;
     }
 }
 
 function checkName(name){
     let words = name.trim().split(" ");
     if (words.length === 2 && words[0].length > 0 && words[1].length > 0){
-        return;
+        return 0;
     }
     else {
-        alert("Must input both first and last name")
+        let invalid = document.createElement("div");
+        invalid.classList.add("alert");
+        document.body.appendChild(invalid);
+        invalid.appendChild( document.createTextNode( "Please enter both your first and last name"));
+        return 1;
     }
 }
 
@@ -43,9 +54,13 @@ function checkAge(bday){
         age--;
     }
     if (age >= 13){
-        return;
+        return 0;
     }
     else{
-        alert("Must be 13 years old or older");
+        let invalid = document.createElement("div");
+        invalid.classList.add("alert");
+        document.body.appendChild(invalid);
+        invalid.appendChild( document.createTextNode( "New users must 13 years of age or older"));
+        return 1;
     }
 }
